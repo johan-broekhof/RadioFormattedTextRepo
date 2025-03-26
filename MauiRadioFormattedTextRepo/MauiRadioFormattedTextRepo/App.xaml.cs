dbe330dfb9851db69bce3994d5a580abc2bb5555
Luuk Broekhof <l.broekhof@idas.nl>
2025-03-26 09:23:33 +0100

    Add project files.

---
+++ MauiRadioFormattedTextRepo/MauiRadioFormattedTextRepo/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiRadioFormattedTextRepo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ MauiRadioFormattedTextRepo/MauiRadioFormattedTextRepo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiRadioFormattedTextRepo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

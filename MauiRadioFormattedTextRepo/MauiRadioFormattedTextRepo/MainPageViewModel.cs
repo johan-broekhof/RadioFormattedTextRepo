60a5368865256eb3c9c64e4fa31491bd0c88885e
Luuk Broekhof <l.broekhof@idas.nl>
2025-03-26 09:27:25 +0100

    create main page view model

---
+++ MauiRadioFormattedTextRepo/MauiRadioFormattedTextRepo/MainPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiRadioFormattedTextRepo;
+
+public class MainPageViewModel
+{
+    private int _count = 30;
+
+    public int Count
+    {
+        get => _count;
+        set => _count = value;
+    }
+}
